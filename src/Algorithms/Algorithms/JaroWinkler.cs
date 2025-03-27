3b8db0d90ca02fbdef3e014fb0a3de4d5dc6c21e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-03-27 11:08:50 +0100

    feat: add new method

---
+++ src/Algorithms/Algorithms/JaroWinkler.cs
@@ -0,0 +1,9 @@
+﻿namespace Algorithms;
+
+public static class JaroWinkler
+{
+    public static double JaroWinklerResult(string s1, string s2)
+    {
+        return 1;
+    }
+}
---
+++ src/Tests/AlgorithmTests/JaroWinklerTests.cs
@@ -0,0 +1,12 @@
+﻿using Algorithms;
+
+namespace AlgorithmTests;
+
+public class JaroWinklerTests
+{
+    [Fact]
+    public void IdenticalWords_ShouldReturn1()
+    {
+        Assert.Equal(1, JaroWinkler.JaroWinklerResult("hello", "hello"));
+    }
+}
