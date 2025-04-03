d08f79a122293af90098d16a70fc9d9e8b37fa14
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-03 11:02:48 +0200

    feat: add method

---
+++ src/Algorithms/Algorithms/DamerauLevenshtein.cs
@@ -0,0 +1,9 @@
+﻿namespace Algorithms;
+
+public class DamerauLevenshtein
+{
+    public static int DamerauLevenshteinResult(string v1, string v2)
+    {
+        return 0;
+    }
+}
---
+++ src/Tests/AlgorithmTests/DamerauLevenshteinTests.cs
@@ -0,0 +1,12 @@
+﻿using Algorithms;
+
+namespace AlgorithmTests;
+
+public class DamerauLevenshteinTests
+{
+    [Fact]
+    public void IdenticalWords_ShouldReturn0()
+    {
+        Assert.Equal(0, DamerauLevenshtein.DamerauLevenshteinResult("hello", "hello"));
+    }
+}
