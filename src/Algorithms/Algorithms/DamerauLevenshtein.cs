77a551be07978750625791298c0490be8ef2447a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-03 12:42:41 +0200

    feat: implement full algorithm

--- src/Algorithms/Algorithms/DamerauLevenshtein.cs
+++ src/Algorithms/Algorithms/DamerauLevenshtein.cs
@@ -4,8 +4,33 @@ public class DamerauLevenshtein
 {
     public static int DamerauLevenshteinResult(string s1, string s2)
     {
-        if (s1 == s2) return 0;
+        var len1 = s1.Length;
+        var len2 = s2.Length;
 
-        return 1;
+        var dp = new int[len1 + 1, len2 + 1];
+
+        for (var i = 0; i < len1; i++) dp[i, 0] = i;
+        for (var i = 0; i < len2; i++) dp[0, i] = i;
+
+        for (var i = 1; i <= len1; i++)
+        {
+            for (var j = 1; j <= len2; j++)
+            {
+                var cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+
+                dp[i, j] = Math.Min(
+                    Math.Min(dp[i - 1, j] + 1,
+                        dp[i, j - 1] + 1),
+                    dp[i - 1, j - 1] + cost
+                );
+
+                if (i > 1 && j > 1 && s1[i - 1] == s2[j - 2] && s1[i - 2] == s2[j - 1])
+                {
+                    dp[i, j] = Math.Min(dp[i, j], dp[i - 2, j - 2] + cost);
+                }
+            }
+        }
+
+        return dp[len1, len2];
     }
 }
--- src/Tests/AlgorithmTests/DamerauLevenshteinTests.cs
+++ src/Tests/AlgorithmTests/DamerauLevenshteinTests.cs
@@ -19,18 +19,27 @@ public class DamerauLevenshteinTests
     [Fact]
     public void OneInsertion_ShouldReturn1()
     {
-        Assert.Equal(1, DamerauLevenshtein.DamerauLevenshteinResult("helo", "hello"));
+        Assert.Equal(1, DamerauLevenshtein.DamerauLevenshteinResult("car", "char"));
     }
 
     [Fact]
     public void OneDeletion_ShouldReturn1()
     {
-        Assert.Equal(1, DamerauLevenshtein.DamerauLevenshteinResult("hello", "helo"));
+        Assert.Equal(1, DamerauLevenshtein.DamerauLevenshteinResult("char", "car"));
     }
 
     [Fact]
     public void OneTransposition_ShouldReturn1()
     {
         Assert.Equal(1, DamerauLevenshtein.DamerauLevenshteinResult("hlelo", "hello"));
+    }
+
+    [Theory]
+    [InlineData("dog", "god", 2)]
+    [InlineData("kitten", "sitting", 3)]
+    [InlineData("abcd", "efgh", 4)]
+    public void MultipleEdits_ShouldReturnCorrectDistance(string s1, string s2, int distance)
+    {
+        Assert.Equal(distance, DamerauLevenshtein.DamerauLevenshteinResult(s1, s2));
     }
 }
